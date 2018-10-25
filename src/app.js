50927ffe50e37cf135a8fd4d8008c901cf52716d
milekag01 <milekag01@gmail.com>
2018-10-26 00:03:27 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(<p>budgetApp</p>,appRoot);
