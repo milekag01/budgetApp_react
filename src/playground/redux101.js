18ac41c680f56061f88f9f798e9db8010ff20859
milekag01 <milekag01@gmail.com>
2018-10-27 17:28:17 +0530

    redux state watch

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -3,12 +3,18 @@ import { createStore } from 'redux';
 const store = createStore((state={count: 0},action) => {
     switch(action.type){
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             }
         case 'DECREMENT':
+        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
+            }
+        case 'SET': 
+            return {
+                count: action.count
             }
         case 'RESET':
             return {
@@ -23,14 +29,20 @@ const store = createStore((state={count: 0},action) => {
 //     this is arrow function as 1st argument of createStore 
 // })
 
-console.log(store.getState());
+
+//store.subscribe() will watch for changing states and it return a unscribe function which can be used after any action to stop watching
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+})
+
 
 
 // Actions - it is an object that gets sent to the store
 // these help us to update our state on the basis of type we are sending to store via store.dispatch()
- 
+
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
 
 store.dispatch({
@@ -42,5 +54,11 @@ store.dispatch({
 store.dispatch({
     type: 'DECREMENT'
 });
-
-console.log(store.getState());
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 3
+});
+store.dispatch({
+    type: 'SET',
+    count: 101
+})
