7e34e209dd4b0421218352273d8c77515216059c
milekag01 <milekag01@gmail.com>
2018-10-28 14:13:20 +0530

    break redux-expensify to different files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const appRoot = document.getElementById('app');
+const store = configureStore();
 
+store.subscribe(() => {
+    const state = store.getState();
+    const VisibleExpenses = getVisibleExpenses(state.expenses,state.filters);
+    console.log(VisibleExpenses);
+})
+
+store.dispatch(addExpense({ description: 'water bill', amount: 100, createdAt: -250 }));
+store.dispatch(addExpense({ description: 'gas bill', amount: 300, createdAt: 123 }));
+
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+
+
+
+const appRoot = document.getElementById('app');
 ReactDOM.render(<AppRouter />,appRoot);
