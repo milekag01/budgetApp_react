c2824c2b4f0ccf9dd7c20d363c30094ffd3b6fda
milekag01 <milekag01@gmail.com>
2018-11-10 00:42:11 +0530

    date range filter

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,32 +1,61 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {setTextFilter, sortByDate, sortByAmount} from '../actions/filters';
+import {setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate} from '../actions/filters';
+import { DateRangePicker } from 'react-dates';
 
-const ExpenseListFilters = (props) => (
-    <div>
-        <input 
-            type="text" 
-            value={props.filters.text}      //this is an example of controlled input where input value is controlled by javascript
-            onChange = {(event) => {
-                props.dispatch(setTextFilter(event.target.value));
-            }}
-        />
+class ExpenseListFilters extends React.Component {
+    state = {
+        calenderFocused: null
+    };
 
-        <select 
-            value={props.filters.sortBy}    //this is an example of controlled input where input value is controlled by javascript
-            onChange={(event) => {
-                if(event.target.value === 'date') {
-                    props.dispatch(sortByDate());
-                } else if(event.target.value === 'amount') {
-                    props.dispatch(sortByAmount());
-                }
-            }}
-        >
-            <option value="date">Date</option>
-            <option value="amount">Amount</option>
-        </select>
-    </div>
-);
+    onDatesChange = ({startDate,endDate}) => {
+        this.props.dispatch(setStartDate(startDate));
+        this.props.dispatch(setEndDate(endDate));
+    }
+
+    onFocusChange = (calenderFocused) => {
+        this.setState (() => ({calenderFocused: calenderFocused}));
+    }
+
+    render() {
+        return (
+            <div>
+                <input 
+                    type="text" 
+                    value={this.props.filters.text}      //this is an example of controlled input where input value is controlled by javascript
+                    onChange = {(event) => {
+                        this.props.dispatch(setTextFilter(event.target.value));
+                    }}
+                />
+
+                <select 
+                    value={this.props.filters.sortBy}    //this is an example of controlled input where input value is controlled by javascript
+                    onChange={(event) => {
+                        if(event.target.value === 'date') {
+                            this.props.dispatch(sortByDate());
+                        } else if(event.target.value === 'amount') {
+                            this.props.dispatch(sortByAmount());
+                        }
+                    }}
+                >
+                    <option value="date">Date</option>
+                    <option value="amount">Amount</option>
+                </select>
+                
+                <DateRangePicker 
+                    startDate = {this.props.filters.startDate}
+                    endDate = {this.props.filters.endDate}
+                    onDatesChange = {this.onDatesChange}
+                    focusedInput = {this.state.calenderFocused}
+                    onFocusChange = {this.onFocusChange}
+                    showClearDates = {true}
+                    numberOfMonths = {1}
+                    isOutsideRange = {() => false}
+                />
+            </div>
+        );
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
