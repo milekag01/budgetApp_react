f1b9361887389f760de9ad19cf348d2d958cfa59
milekag01 <milekag01@gmail.com>
2018-11-12 02:07:19 +0530

    enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+// import toJSON from 'enzyme-to-json';         //since we are using enzyme-to-json/serializer, we do not need to import and use (enzyme-to-json) since it will be used automatically
 import Header from '../../components/Header';
 
 test('should render header correctly',() => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    // expect(toJSON(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
+
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
