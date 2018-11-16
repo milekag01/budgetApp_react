965c175b8c4a9de20787377169f2475496433cb8
milekag01 <milekag01@gmail.com>
2018-11-17 02:38:20 +0530

    using beforeEach middleware

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -7,7 +7,7 @@ export class AddExpensePage extends React.Component {
     onSubmit = (expense) => {
         // console.log(expense);
         //props.dispatch(addExpense(expense));
-        this.props.onSubmit(expense);
+        this.props.addExpense(expense);
         this.props.history.push('/');
     };
     render() {
@@ -23,7 +23,7 @@ export class AddExpensePage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    onSubmit: (expense) => dispatch(addExpense(expense))
+    addExpense: (expense) => dispatch(addExpense(expense))
 });
 
 export default connect(undefined,mapDispatchToProps)(AddExpensePage);
